832be8c65bf0237524bccce16e32e42faa976be9
Tim Ruffing <crypto@timruffing.de>
2024-03-06 10:16:16 +0100

    Run `ruff format`

--- reference/tests.py
+++ reference/tests.py
@@ -6,7 +6,29 @@ import asyncio
 
 from crypto_bip340 import n as GROUP_ORDER, point_mul, G
 from crypto_extra import pubkey_gen_plain
-from reference import secret_share_shard, kdf, vss_verify, vss_commit, vss_sum_commitments, simplpedpop_round1, simplpedpop_pre_finalize, encpedpop_round1, encpedpop_pre_finalize, chilldkg_hostkey_gen, chilldkg_session_params, chilldkg_round1, chilldkg_round2, chilldkg_finalize, chilldkg_recover, CoordinatorChannels, SignerChannel, chilldkg, chilldkg_coordinate, polynomial_evaluate
+from reference import (
+    secret_share_shard,
+    kdf,
+    vss_verify,
+    vss_commit,
+    vss_sum_commitments,
+    simplpedpop_round1,
+    simplpedpop_pre_finalize,
+    encpedpop_round1,
+    encpedpop_pre_finalize,
+    chilldkg_hostkey_gen,
+    chilldkg_session_params,
+    chilldkg_round1,
+    chilldkg_round2,
+    chilldkg_finalize,
+    chilldkg_recover,
+    CoordinatorChannels,
+    SignerChannel,
+    chilldkg,
+    chilldkg_coordinate,
+    polynomial_evaluate,
+)
+
 
 def scalar_add_multi(scalars: List[int]) -> int:
     acc = 0
@@ -14,15 +36,18 @@ def scalar_add_multi(scalars: List[int]) -> int:
         acc = (acc + scalar) % GROUP_ORDER
     return acc
 
+
 def test_vss_correctness():
     def rand_polynomial(t):
         return [randint(1, GROUP_ORDER - 1) for _ in range(1, t + 1)]
+
     for t in range(1, 3):
-        for n in range(t, 2*t + 1):
+        for n in range(t, 2 * t + 1):
             f = rand_polynomial(t)
             shares = secret_share_shard(f, n)
-            assert(len(shares) == n)
-            assert(all(vss_verify(i, shares[i], vss_commit(f)) for i in range(n)))
+            assert len(shares) == n
+            assert all(vss_verify(i, shares[i], vss_commit(f)) for i in range(n))
+
 
 def simulate_simplpedpop(seeds, t):
     n = len(seeds)
@@ -34,13 +59,19 @@ def simulate_simplpedpop(seeds, t):
     vss_commitments_sum = vss_sum_commitments(vss_commitments_ext, t)
     for i in range(n):
         shares_sum = scalar_add_multi([out[2][i] for out in round1_outputs])
-        dkg_outputs += [simplpedpop_pre_finalize(round1_outputs[i][0], vss_commitments_sum, shares_sum)]
+        dkg_outputs += [
+            simplpedpop_pre_finalize(
+                round1_outputs[i][0], vss_commitments_sum, shares_sum
+            )
+        ]
     return dkg_outputs
+
 
 def encpedpop_keys(seed: bytes) -> Tuple[bytes, bytes]:
     my_deckey = kdf(seed, "deckey")
     my_enckey = pubkey_gen_plain(my_deckey)
     return my_deckey, my_enckey
+
 
 def simulate_encpedpop(seeds, t):
     n = len(seeds)
@@ -59,8 +90,13 @@ def simulate_encpedpop(seeds, t):
     vss_commitments_sum = vss_sum_commitments(vss_commitments_ext, t)
     for i in range(n):
         enc_shares_sum = scalar_add_multi([out[2][i] for out in round1_outputs])
-        dkg_outputs += [encpedpop_pre_finalize(round1_outputs[i][0], vss_commitments_sum, enc_shares_sum)]
+        dkg_outputs += [
+            encpedpop_pre_finalize(
+                round1_outputs[i][0], vss_commitments_sum, enc_shares_sum
+            )
+        ]
     return dkg_outputs
+
 
 def simulate_chilldkg(seeds, t):
     n = len(seeds)
@@ -70,7 +106,7 @@ def simulate_chilldkg(seeds, t):
         hostkeys += [chilldkg_hostkey_gen(seeds[i])]
 
     hostpubkeys = [hostkey[1] for hostkey in hostkeys]
-    params, _ = chilldkg_session_params(hostpubkeys, t, b'')
+    params, _ = chilldkg_session_params(hostpubkeys, t, b"")
 
     round1_outputs = []
     for i in range(n):
@@ -85,13 +121,18 @@ def simulate_chilldkg(seeds, t):
         all_enc_shares_sum += [scalar_add_multi([out[2][i] for out in round1_outputs])]
     round2_outputs = []
     for i in range(n):
-        round2_outputs += [chilldkg_round2(seeds[i], state1s[i], vss_commitments_sum, all_enc_shares_sum)]
+        round2_outputs += [
+            chilldkg_round2(
+                seeds[i], state1s[i], vss_commitments_sum, all_enc_shares_sum
+            )
+        ]
 
-    cert = b''.join([out[1] for out in round2_outputs])
+    cert = b"".join([out[1] for out in round2_outputs])
     for i in range(n):
         dkg_outputs += [chilldkg_finalize(round2_outputs[i][0], cert)]
 
     return dkg_outputs
+
 
 def simulate_chilldkg_full(seeds, t):
     n = len(seeds)
@@ -99,20 +140,25 @@ def simulate_chilldkg_full(seeds, t):
     for i in range(n):
         hostkeys += [chilldkg_hostkey_gen(seeds[i])]
 
-    params= chilldkg_session_params([hostkey[1] for hostkey in hostkeys], t, b'')[0]
+    params = chilldkg_session_params([hostkey[1] for hostkey in hostkeys], t, b"")[0]
     hostpubkeys = [hostkey[1] for hostkey in hostkeys]
+
     async def main():
         coord_chans = CoordinatorChannels(n)
         signer_chans = [SignerChannel(coord_chans.queues[i]) for i in range(n)]
         coord_chans.set_signer_queues([signer_chans[i].queue for i in range(n)])
-        coroutines = [chilldkg_coordinate(coord_chans, params)] + [chilldkg(signer_chans[i], seeds[i], hostkeys[i][0], params) for i in range(n)]
+        coroutines = [chilldkg_coordinate(coord_chans, params)] + [
+            chilldkg(signer_chans[i], seeds[i], hostkeys[i][0], params)
+            for i in range(n)
+        ]
         return await asyncio.gather(*coroutines)
 
     outputs = asyncio.run(main())
     # Check coordinator output
-    assert(outputs[0][0] == outputs[1][0][1])
-    assert(outputs[0][1] == outputs[1][0][2])
+    assert outputs[0][0] == outputs[1][0][1]
+    assert outputs[0][1] == outputs[1][0][2]
     return [[out[0][0], out[0][1], out[0][2], out[1]] for out in outputs[1:]]
+
 
 # Adapted from BIP 324
 def scalar_inv(a: int):
@@ -137,46 +183,51 @@ def scalar_inv(a: int):
         t1 += GROUP_ORDER
     return t1
 
+
 def derive_interpolating_value(L, x_i):
-    assert(x_i in L)
-    assert(all(L.count(x_j) <= 1 for x_j in L))
+    assert x_i in L
+    assert all(L.count(x_j) <= 1 for x_j in L)
     numerator = 1
     denominator = 1
     for x_j in L:
-        if x_j == x_i: continue
+        if x_j == x_i:
+            continue
         numerator = (numerator * x_j) % GROUP_ORDER
         denominator = (denominator * ((x_j - x_i) % GROUP_ORDER)) % GROUP_ORDER
     denom_inv = scalar_inv(denominator)
     return (numerator * denom_inv) % GROUP_ORDER
 
+
 def recover_secret(signer_indices, shares):
     interpolated_shares = []
     t = len(shares)
-    assert(len(signer_indices) == t)
+    assert len(signer_indices) == t
     for i in range(t):
         l = derive_interpolating_value(signer_indices, signer_indices[i])
         interpolated_shares += [(l * shares[i]) % GROUP_ORDER]
     recovered_secret = scalar_add_multi(interpolated_shares)
     return recovered_secret
 
+
 def test_recover_secret():
     f = [23, 42]
     shares = [polynomial_evaluate(f, i) for i in [1, 2, 3]]
-    assert(recover_secret([1,2], [shares[0], shares[1]]) == f[0])
-    assert(recover_secret([1,3], [shares[0], shares[2]]) == f[0])
-    assert(recover_secret([2,3], [shares[1], shares[2]]) == f[0])
+    assert recover_secret([1, 2], [shares[0], shares[1]]) == f[0]
+    assert recover_secret([1, 3], [shares[0], shares[2]]) == f[0]
+    assert recover_secret([2, 3], [shares[1], shares[2]]) == f[0]
+
 
 def dkg_correctness(t, n, simulate_dkg, external_eq):
     seeds = [secrets.token_bytes(32) for _ in range(n)]
 
     dkg_outputs = simulate_dkg(seeds, t)
-    assert(all([out != False for out in dkg_outputs]))
+    assert all([out != False for out in dkg_outputs])
     if external_eq:
         # TODO: move into separate function "eta_eq"
         etas = [out[0] for out in dkg_outputs]
-        assert(len(etas) == n)
+        assert len(etas) == n
         for i in range(1, n):
-            assert(etas[0] == etas[i])
+            assert etas[0] == etas[i]
         dkg_outputs = [out[1] for out in dkg_outputs]
 
     shares = [out[0] for out in dkg_outputs]
@@ -185,31 +236,34 @@ def dkg_correctness(t, n, simulate_dkg, external_eq):
 
     # Check that the shared pubkey and signer_pubkeys are the same for all
     # participants
-    assert(len(set(shared_pubkeys)) == 1)
+    assert len(set(shared_pubkeys)) == 1
     shared_pubkey = shared_pubkeys[0]
     for i in range(1, n):
-        assert(signer_pubkeys[0] == signer_pubkeys[i])
+        assert signer_pubkeys[0] == signer_pubkeys[i]
 
     # Check that the share corresponds to the signer_pubkey
     for i in range(n):
-        assert(point_mul(G, shares[i]) == signer_pubkeys[0][i])
+        assert point_mul(G, shares[i]) == signer_pubkeys[0][i]
 
     # Check that the first t signers (TODO: should be an arbitrary set) can
     # recover the shared pubkey
-    recovered_secret = recover_secret(list(range(1, t+1)), shares[0:t])
-    assert(point_mul(G, recovered_secret) == shared_pubkey)
+    recovered_secret = recover_secret(list(range(1, t + 1)), shares[0:t])
+    assert point_mul(G, recovered_secret) == shared_pubkey
 
     # test correctness of chilldkg_recover
-    if len (dkg_outputs[0]) > 3:
+    if len(dkg_outputs[0]) > 3:
         for i in range(n):
-            (share, shared_pubkey_, signer_pubkeys_), _ = chilldkg_recover(seeds[i], dkg_outputs[i][3], b'')
-            assert(share == shares[i])
-            assert(shared_pubkey_ == shared_pubkeys[i])
-            assert(signer_pubkeys_ == signer_pubkeys[i])
+            (share, shared_pubkey_, signer_pubkeys_), _ = chilldkg_recover(
+                seeds[i], dkg_outputs[i][3], b""
+            )
+            assert share == shares[i]
+            assert shared_pubkey_ == shared_pubkeys[i]
+            assert signer_pubkeys_ == signer_pubkeys[i]
+
 
 test_vss_correctness()
 test_recover_secret()
-for (t, n) in [(1, 1), (1, 2), (2, 2), (2, 3), (2, 5)]:
+for t, n in [(1, 1), (1, 2), (2, 2), (2, 3), (2, 5)]:
     external_eq = True
     dkg_correctness(t, n, simulate_simplpedpop, external_eq)
     dkg_correctness(t, n, simulate_encpedpop, external_eq)
--- reference/util.py
+++ reference/util.py
@@ -3,15 +3,19 @@ class InvalidContributionError(Exception):
         self.signer = signer
         self.contrib = error
 
+
 class InvalidBackupError(Exception):
     pass
+
 
 class DeserializationError(Exception):
     pass
 
+
 class VSSVerifyError(Exception):
     def __init__(self):
         pass
+
 
 class DuplicateHostpubkeyError(Exception):
     def __init__(self):
