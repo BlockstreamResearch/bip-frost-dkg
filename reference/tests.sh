8a8441490929be05eebd9de05ecb19343095342a
Tim Ruffing <crypto@timruffing.de>
2024-03-07 18:10:38 +0100

    Add ruff to tests.sh

--- reference/tests.sh
+++ reference/tests.sh
@@ -5,5 +5,10 @@ set -e
 command -v mypy >/dev/null 2>&1 || { echo >&2 "mypy is required but it's not installed.  Aborting."; exit 1; }
 
 cd "$(dirname "$0")"
-mypy --no-error-summary .
+
+# Keep going if a linter fails
+ruff check || true
+ruff format --diff --quiet || true
+mypy --no-error-summary . || true
+
 python3 tests.py
