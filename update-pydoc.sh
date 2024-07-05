cba3a37e36bce5f0eb61a98228743f7cb7c85614
Tim Ruffing <crypto@timruffing.de>
2024-07-05 13:40:07 +0200

    update-pydoc: set -euo pipefail

--- update-pydoc.sh
+++ update-pydoc.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 # This needs GNU seed with support for -z.
 
@@ -10,10 +12,11 @@ pydoc-markdown -I python/chilldkg_ref -m chilldkg |
     # Remove <a> elements
     sed -z 's|<a id="[^"]*"></a>\n\n||g' |
     # Replace bold (**) by italics (*)
-    sed -z 's/\*\*/*/g' > pydoc.md &&
-    # Clean README.md
-    sed -z -i README.md -e 's/<!--pydoc.md-->.*<!--end of pydoc.md-->/<!--pydoc.md-->\n<!--end of pydoc.md-->/' &&
-    # Insert pydoc.md into README
-    sed -i README.md -e '/<!--pydoc.md-->/r pydoc.md' &&
-    # Remove temporary file
-    rm pydoc.md
+    sed -z 's/\*\*/*/g' > pydoc.md
+
+# Clean README.md
+sed -z -i README.md -e 's/<!--pydoc.md-->.*<!--end of pydoc.md-->/<!--pydoc.md-->\n<!--end of pydoc.md-->/'
+# Insert pydoc.md into README
+sed -i README.md -e '/<!--pydoc.md-->/r pydoc.md'
+# Remove temporary file
+rm pydoc.md
