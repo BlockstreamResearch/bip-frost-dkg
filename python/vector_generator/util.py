02800351097f33f8c99c73d1f7a23838592bf512
siv2r <siv2ram@gmail.com>
2025-03-30 14:21:31 +0530

    python: test generated vectors against the APIs

--- python/vector_generator/util.py
+++ python/vector_generator/util.py
@@ -159,3 +159,88 @@ def cinv_msg_asdict(cinv_msg: CoordinatorInvestigationMsg) -> dict:
         "enc_partial_secshares": bytes_list_to_hex(enc_partial_secshares),
         "partial_pubshares": bytes_list_to_hex(partial_pubshares),
     }
+
+
+# functions below are used to test JSON vectors with chilldkg_ref
+# in tests.py
+
+
+def assert_raises(try_fn, expected_error: dict):
+    try:
+        try_fn()
+    except Exception as e:
+        assert expected_error == exception_asdict(e)
+    else:
+        raise AssertionError("Expected exception")
+
+
+def params_from_dict(params: dict) -> SessionParams:
+    return SessionParams(
+        hex_list_to_bytes(params["hostpubkeys"]),
+        params["t"],
+    )
+
+
+def pmsg1_from_dict(pmsg1: dict) -> ParticipantMsg1:
+    pop = simplpedpop.Pop(bytes.fromhex(pmsg1["simpl_pmsg"]["pop"]))
+    com = bytes.fromhex(pmsg1["simpl_pmsg"]["com"])
+    pubnonce = bytes.fromhex(pmsg1["pubnonce"])
+    enc_shares = hex_list_to_bytes(pmsg1["enc_shares"])
+
+    t, remainder = divmod(len(com), 33)
+    if remainder != 0:
+        raise ValueError
+    simpl_pmsg = simplpedpop.ParticipantMsg(VSSCommitment.from_bytes_and_t(com, t), pop)
+
+    enc_pmsg = encpedpop.ParticipantMsg(
+        simpl_pmsg, pubnonce, [Scalar.from_bytes(share) for share in enc_shares]
+    )
+
+    return chilldkg.ParticipantMsg1(enc_pmsg)
+
+
+def pmsg2_from_dict(pmsg2: dict) -> ParticipantMsg2:
+    sig = bytes.fromhex(pmsg2["sig"])
+    return chilldkg.ParticipantMsg2(sig)
+
+
+def cmsg1_from_dict(cmsg1: dict) -> CoordinatorMsg1:
+    coms_to_secrets = [
+        GE.from_bytes_with_infinity(b)
+        for b in hex_list_to_bytes(cmsg1["simpl_cmsg"]["coms_to_secrets"])
+    ]
+    sum_coms_to_nonconst_terms = [
+        GE.from_bytes_with_infinity(b)
+        for b in hex_list_to_bytes(cmsg1["simpl_cmsg"]["sum_coms_to_nonconst_terms"])
+    ]
+    pops = [simplpedpop.Pop(b) for b in hex_list_to_bytes(cmsg1["simpl_cmsg"]["pops"])]
+    pubnonces = hex_list_to_bytes(cmsg1["pubnonces"])
+    enc_secshares = [
+        Scalar.from_bytes(share) for share in hex_list_to_bytes(cmsg1["enc_secshares"])
+    ]
+
+    simpl_cmsg = simplpedpop.CoordinatorMsg(
+        coms_to_secrets, sum_coms_to_nonconst_terms, pops
+    )
+    enc_cmsg = encpedpop.CoordinatorMsg(simpl_cmsg, pubnonces)
+    return chilldkg.CoordinatorMsg1(enc_cmsg, enc_secshares)
+
+
+def cmsg2_from_dict(cmsg2: dict) -> CoordinatorMsg2:
+    cert = bytes.fromhex(cmsg2["cert"])
+    return chilldkg.CoordinatorMsg2(cert)
+
+
+def cinv_msg_from_dict(cinv_msg: dict) -> CoordinatorInvestigationMsg:
+    enc_partial_secshares = [
+        Scalar.from_bytes(share)
+        for share in hex_list_to_bytes(cinv_msg["enc_partial_secshares"])
+    ]
+    partial_pubshares = [
+        GE.from_bytes_with_infinity(b)
+        for b in hex_list_to_bytes(cinv_msg["partial_pubshares"])
+    ]
+    enc_cinv = encpedpop.CoordinatorInvestigationMsg(
+        enc_partial_secshares, partial_pubshares
+    )
+    return chilldkg.CoordinatorInvestigationMsg(enc_cinv)
